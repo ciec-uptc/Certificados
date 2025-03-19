3fc456ae4e98b9522834b1ebf303b1f7f882f739
ciec-uptc <ciec@uptc.edu.co>
2025-03-18 21:39:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ st.title("Certificados Automáticos")
 st.write("¡La aplicación está funcionando correctamente!")
 
 if st.button("Probar Aplicación"):
-    st.success("✅ Streamlit está funcionando.")
+    st.success("✅ Streamlit está funcionando correctamente.")
